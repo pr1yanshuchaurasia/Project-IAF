0128d8eb5c0fc496d93656c1d97f0871fa1ed396
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2026-09-08 22:37:37 +0530

    refactor: Simplify route definitions in App component for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,29 @@ import Signup from "./components/Signup";
 import Dashboard from "./pages/Dashboard";
 import Fighters from "./pages/Fighters";
 import FutureAircraft from "./pages/FutureAircraft";
+import CommandCenter from "./pages/CommandCenter";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Navigation />
 
       <Routes>
-
         <Route path="/" element={<Home />} />
 
-        <Route
-          path="/login"
-          element={<Login />}
-        />
+        <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/signup"
-          element={<Signup />}
-        />
+        <Route path="/signup" element={<Signup />} />
 
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route
-          path="/fighters"
-          element={<Fighters />}
-        />
+        <Route path="/fighters" element={<Fighters />} />
 
-        <Route
-          path="/future"
-          element={<FutureAircraft />}
-        />
+        <Route path="/future" element={<FutureAircraft />} />
 
+        <Route path="/command-center" element={<CommandCenter />} />
+        
       </Routes>
-
     </BrowserRouter>
   );
 }
